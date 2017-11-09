9197026b88a1eaf1c0cad6dd99efc5b944405a46
ncsa-mo <ondrejce@illinois.edu>
2017-11-08 18:20:39 -0600

    add match_contacts and search_by_contacts

--- mdf_forge/forge.py
+++ mdf_forge/forge.py
@@ -386,6 +386,30 @@ class Forge:
                              new_group=False)
         return self
 
+    def match_contacts(self, contacts):
+        """Add titles to the query.
+
+        Arguments:
+        contacts (str or list of str): The contact names to match.
+
+        Returns:
+        self (Forge): For chaining.
+        """
+
+        if not contacts:
+            print_("Error: No contact specified.")
+            return self
+        if not isinstance(contacts, list):
+            contacts = [contacts]
+        if '' in contacts:
+            print_("Error: No contact specified.")
+            return self
+
+        self.match_field(field="mdf.data_contact", value=contacts[0], required=True, new_group=True)
+        for contact in contacts[1:]:
+            self.match_field(field="mdf.data_contact", value=contact, required=False, new_group=False)
+        return self
+
 
     def match_resource_types(self, types):
         """Match the given resource types.
@@ -439,6 +463,24 @@ class Forge:
         return (self.match_elements(elements, match_all=match_all)
                     .match_sources(sources)
                     .search(limit=limit, info=info))
+
+
+    def search_by_contacts(self, contacts=[], limit=None, info=False):
+        """Execute a search for the given contact names.
+        search_by_contacts([x]) is equivalent to match_contacts([x]).search()
+
+        Arguments:
+        contacts (list of str): The contact names to match. Default [].
+        limit (int): The maximum number of results to return. The max for this argument is the SEARCH_LIMIT imposed by Globus Search.
+        info (bool): If False, search will return a list of the results.
+        If True, search will return a tuple containing the results list, and other information about the query.
+        Default False.
+
+        Returns:
+        list (if info=False): The results.
+        tuple (if info=True): The results, and a dictionary of query information.
+        """
+        return self.match_contacts(contacts).search(limit=limit, info=info)
 
 
     def aggregate_source(self, sources):
